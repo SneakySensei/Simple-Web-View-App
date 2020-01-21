ce19a863003dbe65cfd2f3c62202d67e3a6fb756
Snehil <snehilwebber@gmail.com>
2020-01-21 09:13:41 +0530

    Simple Web View App

---
+++ app/src/main/java/com/novus/trafficsense/Web.java
@@ -0,0 +1,73 @@
+package com.novus.trafficsense;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Web extends AppCompatActivity {
+
+    private WebView webView;
+
+    private LinearLayout spinner;
+    String ShowOrHideWebViewInitialUse = "show";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        webView = findViewById(R.id.webview);
+
+        spinner = findViewById(R.id.progressBar);
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        webView.setWebViewClient(new CustomWebViewClient());
+
+//        webView.loadUrl("http://192.168.43.183");
+        webView.loadUrl("http://192.168.80.80");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                if(webView.canGoBack() == true)
+
+                    webView.goBack();
+                else finish();
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+            if (ShowOrHideWebViewInitialUse.equals("show")) {
+                webview.setVisibility(webview.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            ShowOrHideWebViewInitialUse = "hide";
+
+            spinner.setVisibility(View.GONE);
+
+            view.setVisibility(webView.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
+    }
+}
+
+
